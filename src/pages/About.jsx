3d357c1ed6e535a2292e4cd067068903ad8f390e
Muhammad Bilal <muhammadbilal91055.com>
2024-03-31 17:12:13 +0500

    About section's width adjusted

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -20,7 +20,7 @@ function About() {
           I am a developer with experience in React.js, as well as a range of other skills including frontend development, JavaScript, and Tailwind. I have completed several individual projects and am always looking for new opportunities and challenges to improve my skills and advance my career.
           </span>
         </div>
-        <div className="flex  flex-col md:flex-row md:justify-evenly justify-center gap-4 items-center  mx-auto w-10/12 py-10">
+        <div className="flex  flex-col md:flex-row md:justify-evenly justify-center gap-4 items-center  mx-auto w-10/12 pt-20">
           <IntroServices img={Developer4_img} title="Web Developer" />
           <IntroServices img={react_logo} title="React Developer" />
           <IntroServices img={UX_Designer} title="UX Designer" />
